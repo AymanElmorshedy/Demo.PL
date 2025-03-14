5f151fc8e6f280eafc7e94c55bff7dee63fd26f7
ayman <aymanehab2002@gmail.com>
2025-03-15 01:46:40 +0200

    Add project files.

---
+++ Demo.BLL/Repository/DepartmentRepository.cs
@@ -0,0 +1,37 @@
+﻿using Demo.DAL.Context;
+using Demo.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.BLL.Repository
+{
+    public class DepartmentRepository
+    {
+        private readonly AppDbContext dbContext;
+
+        //GetAll Department, GetById, Delete, Create, Update
+        //Dependancy Injection
+        //FirstWay
+        //[FromService]
+        //Prop
+        //Second Way
+        //Function([FromService]arg)
+        //Third Way
+        //Constructor
+
+        public DepartmentRepository(AppDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+        public List<Department> GetAll()
+        {
+       
+            var Departments=dbContext.Departments.ToList();
+            return Departments;
+        }
+
+    }
+}
